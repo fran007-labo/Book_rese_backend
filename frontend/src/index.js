26494bc00d5bef3f8931e7d3323757354339d0b5
fran007-labo <fran.seagal1@gmail.com>
2022-03-30 18:51:00 +0900

    edit frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+// import Album from './Album';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <Album />, document.getElementById('root')
+  <App />, document.getElementById('root')
 );
