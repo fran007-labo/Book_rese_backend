30a1fabc04075d889bd4db4bfbf6c41ec2566a84
fran007-labo <fran.seagal1@gmail.com>
2022-04-02 12:28:27 +0900

    user/controller 編集

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,26 +1,47 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show destroy update]
+
   def index
-    render json: User.all
-  end
-   
-  def show
-    render json: User.find(params[:id])
+    users = User.all
+    render json: users
   end
 
-  def new
-    @user = User.new
-    render json: { status: 200, message: "Hello World!"}
-  end 
+  def show
+    render json: @user
+  end
 
   def create
-    @user = User.new(user_parameter)
-    if @client.save
-      redirect_to @client
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: @user
+    else
+      render json: @user.errors
     end
   end
 
   private
-  def user_parameter
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def post_params
     params.require(:user).permit(:name, :email)
   end
 
