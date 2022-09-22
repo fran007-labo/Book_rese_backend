535a7e55a8d6908bfa31d8dc527a8d3eec27413e
Yutaka Nagasaki <fran.seagal1@gmail.com>
2022-09-22 14:40:04 +0900

    本番環境用のdockerを編集

--- app/controllers/api/health_check_controller.rb
+++ app/controllers/api/health_check_controller.rb
@@ -1,4 +1,5 @@
 class Api::HealthCheckController < ApplicationController
+  skip_before_action :authenticate_user
   def index
     logger.info "============================================================"
     logger.info "ReuqestUrl: #{request.url}"
