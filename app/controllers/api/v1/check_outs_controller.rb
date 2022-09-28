e90b59f55aeb1575173c51b45c9dc53395e2711d
Yutaka Nagasaki <fran.seagal1@gmail.com>
2022-09-28 21:31:16 +0900

    checkout機能追加完了

--- app/controllers/api/v1/check_outs_controller.rb
+++ app/controllers/api/v1/check_outs_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::CheckOutsController < ApplicationController
 
   def create
-    render status: 200
+    cart = current_user.prepare_cart
+    books = cart.books.ids
+
+    begin
+      books.each do | id |
+        Reservation.create!(
+          book_id: id, 
+          user_id: current_user.id, 
+          start_date: params[:start_date],
+          return_date: params[:return_date]
+        )
+      end
+      flash_message = "本の予約が完了しました。"
+      flash_status = 'success'
+      cart.destroy!
+    rescue => e 
+      flash_message = "本の予約に失敗しました。"
+      flash_status = 'error'
+    end
+
+    render json: {
+      message: flash_message,
+      status: flash_status
+    }
   end
 end
