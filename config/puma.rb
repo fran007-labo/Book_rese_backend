b98fb02a0d81bb0f48c490f27dd9934471df8378
Yutaka Nagasaki <fran.seagal1@gmail.com>
2022-09-28 16:09:44 +0900

    update

--- config/puma.rb
+++ config/puma.rb
@@ -20,4 +20,6 @@ plugin :tmp_restart
 app_root = '/myapp'
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+unless ENV.fetch("RAILS_ENV", "development") == "development"
+  stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+end
