d688423a2d21728f7dbe8ba85a38d4c9797c30dc
Yutaka Nagasaki <fran.seagal1@gmail.com>
2022-10-14 10:54:16 +0900

    aws/redis 設定

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,4 +1,10 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new
+  case Rails.env
+  when 'production' then
+    config.redis = Redis.new(host: ENV['REDIS_URL'], port: 6379)
+  else
+    config.redis = Redis.new
+  end
+
   config.project_ids = [ENV['FIREBASE_PROJECT_ID']]
 end
